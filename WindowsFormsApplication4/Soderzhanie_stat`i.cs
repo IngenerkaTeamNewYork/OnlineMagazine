5d5aaa53a6d608b3b96eb9f6d939b9299191b88a
sosiska2281337 <danhoh22895@gmail.com>
2018-09-15 16:37:11 +0300

    структура
    Добавили структуру статьи , не можем загрузить картинку

---
+++ WindowsFormsApplication4/Soderzhanie_stat`i.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public struct statiya
+    {
+        public String  name_statiya;
+        public List<Image> kartinki_statii;
+        public string kategorita_statii;
+        public String text_statii;
+
+        public void ShowText()
+        {
+            MessageBox.Show("1");
+        }
+    }
+
+
+}
