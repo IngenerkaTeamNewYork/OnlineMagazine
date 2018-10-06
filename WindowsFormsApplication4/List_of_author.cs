00f8cb92b3dbf2c44a74a206f2fe7d3da5ec6839
Zara28 <31924139+Zara28@users.noreply.github.com>
2018-10-06 17:32:38 +0400

    Списки авторов, категориях и
    MessageBox для пользователей

--- WindowsFormsApplication4/List_of_author.cs
+++ WindowsFormsApplication4/List_of_author.cs
@@ -8,26 +8,46 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
 namespace WindowsFormsApplication4
 {
     public partial class List_of_author : Form
     {
-        public static List<AuthorStat> stat = new List<AuthorStat>();
+       
 
         public List_of_author(List<AuthorStat> writers)
         {
             InitializeComponent();
             int uy = 0;
-            foreach(AuthorStat write in writers)
+            String connString = "SslMode=none;" +
+                "Server=db4free.net;" +
+                "database=ingenerka;port=3306;uid=ingenerka;pwd=Beavis1989;old guids=true;";
+            MySqlConnection conn = new MySqlConnection(connString);
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand("SELECT UserName FROM `Authors`", conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+           
+            while (rdr.Read())
             {
                 LinkLabel linklabel1 = new LinkLabel();
-                linklabel1.Size = new Size (70, 20); 
-                linklabel1.Location = new Point (50, 10 + 30*uy);
-                linklabel1.Text = write.name;
+                linklabel1.Size = new Size(70, 20);
+                linklabel1.Location = new Point(50, 10 + 30 * uy);
+                linklabel1.Text = rdr[0].ToString();
 
                 panel1.Controls.Add(linklabel1);
                 uy++;
+               /* MessageBox.Show("Login = " + rdr[0].ToString() +
+                    " Parol = " + rdr[1].ToString());*/
             }
+            rdr.Close();
+
+
+            conn.Close();
+            
 
         }
 
@@ -38,7 +58,7 @@ namespace WindowsFormsApplication4
 
         private void button1_Click(object sender, EventArgs e)
         {
-            StastisticsForm form1 = new StastisticsForm(stat[0]);
+            StastisticsForm form1 = new StastisticsForm(/*StastisticsForm.stat[0]*/);
             form1.ShowDialog();
         }
     }
