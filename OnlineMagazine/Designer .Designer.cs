ad8654bd85ce4865919304d63999ee65f6fe0073
sosiska2281337 <danhoh22895@gmail.com>
2018-12-12 17:03:36 +0400

    Тупа редактор формы

--- OnlineMagazine/Designer .Designer.cs
+++ OnlineMagazine/Designer .Designer.cs
@@ -28,136 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.AuthorsLabel = new System.Windows.Forms.Label();
+            this.categoryTextBox = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.reklamatextBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.podborkaLabel = new System.Windows.Forms.Label();
+            this.PodborkatextBox = new System.Windows.Forms.TextBox();
+            this.authorsTextBox = new System.Windows.Forms.TextBox();
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // AuthorsLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, -2);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(222, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Введи кол-во авторов и прочей дребедени";
+            this.AuthorsLabel.AutoSize = true;
+            this.AuthorsLabel.Location = new System.Drawing.Point(3, 0);
+            this.AuthorsLabel.Name = "AuthorsLabel";
+            this.AuthorsLabel.Size = new System.Drawing.Size(85, 13);
+            this.AuthorsLabel.TabIndex = 0;
+            this.AuthorsLabel.Text = "Кол-во авторов";
             // 
-            // textBox1
+            // categoryTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(6, 62);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.categoryTextBox.Location = new System.Drawing.Point(179, 41);
+            this.categoryTextBox.Name = "categoryTextBox";
+            this.categoryTextBox.Size = new System.Drawing.Size(100, 20);
+            this.categoryTextBox.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(153, 158);
+            this.button1.Location = new System.Drawing.Point(3, 193);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(137, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Изменить";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox2
+            // reklamatextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(6, 152);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
+            this.reklamatextBox.Location = new System.Drawing.Point(179, 117);
+            this.reklamatextBox.Name = "reklamatextBox";
+            this.reklamatextBox.Size = new System.Drawing.Size(100, 20);
+            this.reklamatextBox.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 124);
+            this.label2.Location = new System.Drawing.Point(3, 114);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(215, 13);
+            this.label2.Size = new System.Drawing.Size(137, 26);
             this.label2.TabIndex = 4;
             this.label2.Text = "Введи число рекламы на главной форме";
             // 
-            // label3
+            // podborkaLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 85);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "label3";
+            this.podborkaLabel.AutoSize = true;
+            this.podborkaLabel.Location = new System.Drawing.Point(3, 76);
+            this.podborkaLabel.Name = "podborkaLabel";
+            this.podborkaLabel.Size = new System.Drawing.Size(57, 13);
+            this.podborkaLabel.TabIndex = 5;
+            this.podborkaLabel.Text = "Подборка";
             // 
-            // textBox3
+            // PodborkatextBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(6, 101);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 6;
+            this.PodborkatextBox.Location = new System.Drawing.Point(179, 79);
+            this.PodborkatextBox.Name = "PodborkatextBox";
+            this.PodborkatextBox.Size = new System.Drawing.Size(100, 20);
+            this.PodborkatextBox.TabIndex = 6;
+            this.PodborkatextBox.TextChanged += new System.EventHandler(this.PodborkatextBox_TextChanged);
             // 
-            // textBox4
+            // authorsTextBox
             // 
-            this.textBox4.Location = new System.Drawing.Point(6, 20);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 7;
+            this.authorsTextBox.Location = new System.Drawing.Point(179, 3);
+            this.authorsTextBox.Name = "authorsTextBox";
+            this.authorsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.authorsTextBox.TabIndex = 7;
             // 
-            // label4
+            // categoryLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 43);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "label4";
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Location = new System.Drawing.Point(3, 38);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(60, 13);
+            this.categoryLabel.TabIndex = 8;
+            this.categoryLabel.Text = "Категория";
             // 
-            // button2
+            // tableLayoutPanel1
             // 
-            this.button2.Location = new System.Drawing.Point(16, 120);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Изменить";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.AuthorsLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.textBox5, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.authorsTextBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.reklamatextBox, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.PodborkatextBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.categoryLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.podborkaLabel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.categoryTextBox, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(352, 230);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(179, 155);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
             // Designer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(237, 184);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(352, 230);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Designer";
-            this.Text = "Form1";
+            this.Text = "Настройки внешнего вида и Мавроди";
             this.Load += new System.EventHandler(this.Designer_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label AuthorsLabel;
+        private System.Windows.Forms.TextBox categoryTextBox;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox reklamatextBox;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label podborkaLabel;
+        private System.Windows.Forms.TextBox PodborkatextBox;
+        private System.Windows.Forms.TextBox authorsTextBox;
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
